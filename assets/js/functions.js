978d0bee1a8d04e5a8c21b315064ed9e67ce374a
Saad88888 <saad_baz@yahoo.com>
2022-01-04 18:52:51 -0500

    first commit

---
+++ assets/js/functions.js
@@ -0,0 +1,44 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+
+}
+
+
+$( document ).ready(function() {
+	
+	function nav(){
+		
+		$('.nav-toggle').click(function(){
+			
+			$('.nav').toggleClass('open');
+			
+		});
+		
+	}
+	
+
+
+nav();
+
+
+});
